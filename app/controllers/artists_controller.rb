50bd15dd0727734676f8b79dc5d688b977236293
Dan Roberts <dan@adadevelopersacademy.org>
2016-10-13 22:49:35 -0700

    Prep for controller testing live code:
    Generate an Artist controller and add action stubs
    Add artist routes
    Add views for artist->[index, show, new, edit]

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,26 @@
+class ArtistsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+private
+  def artist_params
+  end
+end
